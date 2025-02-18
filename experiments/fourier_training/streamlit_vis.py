d3a9710cec51ccaef261f0c9aab00260c7c2202d
Henry Scheible <henry.scheible@gmail.com>
2025-02-18 18:38:39 -0500

    Add streamlit app to view bulk training results (#26)
    * final bulk transformer results processing
    
    * add streamlit usage instructions

--- experiments/fourier_training/streamlit_vis.py
+++ experiments/fourier_training/streamlit_vis.py
@@ -1,5 +1,9 @@
+import json
+
 import pandas as pd
+import plotly.express as px
 import streamlit as st
+import torch
 
 st.title("Fourier Component Training Results")
 
@@ -18,10 +22,46 @@ metadata_df = get_metadata_df()
 events_df = get_events_df()
 
 # Display the dataframe
-st.write("Training Runs:")
-selection = st.dataframe(metadata_df, selection_mode="single-row", on_select="rerun")
+st.write("Training Runs: (SELECT ONE)")
+
+selection = st.dataframe(
+    metadata_df.sort_values(by="max_eval_acc", ascending=False).drop_duplicates(
+        ["function_uuid"]
+    ),
+    selection_mode="single-row",
+    on_select="rerun",
+)
 if len(selection["selection"]["rows"]) > 0:  # type: ignore
     row = selection["selection"]["rows"][0]  # type: ignore
-    st.write(metadata_df.loc[selection["selection"]["rows"], "uuid"])  # type: ignore
+    uuid = metadata_df.loc[selection["selection"]["rows"], "run_uuid"].item()  # type: ignore
+    st.write(uuid)
 
-    st.dataframe(events_df)
+    st.dataframe(events_df.loc[events_df["uuid"] == uuid])
+
+    comps = json.loads(metadata_df.loc[row, "comps"])  # type: ignore
+    coeffs = json.loads(metadata_df.loc[row, "coeffs"])  # type: ignore
+
+    st.header("Function Components")
+    st.plotly_chart(
+        px.imshow(
+            torch.tensor(comps), y=[f"Component 1: {coeff:.2f}" for coeff in coeffs]
+        )
+    )
+
+    st.header("Model Performance")
+
+    st.plotly_chart(
+        px.line(
+            events_df.loc[events_df["uuid"] == uuid],
+            x="epoch",
+            y=[
+                "loss",
+                "train_acc",
+                "eval_acc",
+                "eval_acc_0",
+                "eval_acc_1",
+                "eval_acc_2",
+                "eval_acc_3",
+            ],
+        )
+    )
