4d41c13acb78eb5314b50c12e3e0dd5e7b6108ea
Henry Scheible <henry.scheible@gmail.com>
2025-02-23 19:58:16 -0500

    save weights per epoch (#48)

--- src/miexp/models/btransformer.py
+++ src/miexp/models/btransformer.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Any, Self
 
 import pydantic
@@ -19,7 +20,7 @@ class SaveableModule(nn.Module):
         Args:
             checkpoint_path (str): Path to save model to
         """
-        state_dict = self.state_dict()
+        state_dict = copy.deepcopy(self.state_dict())
         torch.save(
             {"hyperparameters": self.hyperparameters, "state_dict": state_dict},
             checkpoint_path,
